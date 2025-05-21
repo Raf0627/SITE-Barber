0c51e76e94e9c6d95348eedc8df0917dbbcc4b19
Raf0627 <rafaelfrossard076@gmail.com>
2025-05-20 22:53:54 -0300

    gradiente posicionado

--- paginas/home.php
+++ paginas/home.php
@@ -1,1 +1,6 @@
-<div class="bem-vindo"><p>Bem-vindo</p></div>
+<div class="background"><img src="/SITE-Barber/imgs/barbearia.jpg" alt=""></div>
+<main>
+    <div class="bem-vindo"><p>Bem-vindo</p></div>
+    <div class="gradiente"></div>
+    <div class="servicos-home"></div>
+</main>
--- paginas/servicos.php
+++ paginas/servicos.php
@@ -1,51 +1,55 @@
+<div class="background"><img src="/SITE-Barber/imgs/barbearia.jpg" alt=""></div>
 <main class="main-servicos">
 <div class="bem-vindo"><p>Serviços</p></div>
-<div class="card col-12 col-md-4" style="width: 18rem;">
-  <img src="/SITE-Barber/imgs/Completo.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Cabelo + Barba + Sobrancelha</h5>
-    <p class="card-text">R$90,00</p>
-    <a href="#" class="btn btn-primary">Agendar</a>
+    <div class="gradiente"></div>
+<div class="all-cards">
+  <div class="card col-12 col-md-4" style="width: 18rem;">
+    <img src="/SITE-Barber/imgs/Completo.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Cabelo + Barba + Sobrancelha</h5>
+      <p class="card-text">R$90,00</p>
+      <a href="#" class="btn btn-primary">Agendar</a>
+    </div>
   </div>
-</div>
-<div class="card col-12 col-md-4" style="width: 18rem;">
-  <img src="/SITE-Barber/imgs/CabeloBarba.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Cabelo + Barba</h5>
-    <p class="card-text">R$80,00</p>
-    <a href="#" class="btn btn-primary">Agendar</a>
+  <div class="card col-12 col-md-4" style="width: 18rem;">
+    <img src="/SITE-Barber/imgs/CabeloBarba.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Cabelo + Barba</h5>
+      <p class="card-text">R$80,00</p>
+      <a href="#" class="btn btn-primary">Agendar</a>
+    </div>
   </div>
-</div>
-<div class="card col-12 col-md-4" style="width: 18rem;">
-  <img src="/SITE-Barber/imgs/CabeloSobrancelha.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Cabelo + Sobrancelha</h5>
-    <p class="card-text">R$60,00</p>
-    <a href="#" class="btn btn-primary">Agendar</a>
+  <div class="card col-12 col-md-4" style="width: 18rem;">
+    <img src="/SITE-Barber/imgs/CabeloSobrancelha.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Cabelo + Sobrancelha</h5>
+      <p class="card-text">R$60,00</p>
+      <a href="#" class="btn btn-primary">Agendar</a>
+    </div>
   </div>
-</div>
-<div class="card col-12 col-md-4" style="width: 18rem;">
-  <img src="/SITE-Barber/imgs/Cabelo.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Cabelo</h5>
-    <p class="card-text">R$45,00</p>
-    <a href="#" class="btn btn-primary">Agendar</a>
+  <div class="card col-12 col-md-4" style="width: 18rem;">
+    <img src="/SITE-Barber/imgs/Cabelo.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Cabelo</h5>
+      <p class="card-text">R$45,00</p>
+      <a href="#" class="btn btn-primary">Agendar</a>
+    </div>
   </div>
-</div>
-<div class="card col-12 col-md-4" style="width: 18rem;">
-  <img src="/SITE-Barber/imgs/Barba.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Barba</h5>
-    <p class="card-text">R$40,00</p>
-    <a href="#" class="btn btn-primary">Agendar</a>
+  <div class="card col-12 col-md-4" style="width: 18rem;">
+    <img src="/SITE-Barber/imgs/Barba.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Barba</h5>
+      <p class="card-text">R$40,00</p>
+      <a href="#" class="btn btn-primary">Agendar</a>
+    </div>
   </div>
-</div>
-<div class="card col-12 col-md-4" style="width: 18rem;">
-  <img src="/SITE-Barber/imgs/Sobrancelha.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Sobrancelha</h5>
-    <p class="card-text">R$20,00</p>
-    <a href="#" class="btn btn-primary">Agendar</a>
+  <div class="card col-12 col-md-4" style="width: 18rem;">
+    <img src="/SITE-Barber/imgs/Sobrancelha.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Sobrancelha</h5>
+      <p class="card-text">R$20,00</p>
+      <a href="#" class="btn btn-primary">Agendar</a>
+    </div>
   </div>
 </div>
     </main>
